27f4eb9c85e9d1b2b336825e5d13e1f3d46b39d9
Simon Spannagel <simon.spannagel@desy.de>
2015-11-04 13:20:35 +0100

    Make submission more flexible, submit 250 jobs

--- generation/runPowhegNaf.sh
+++ generation/runPowhegNaf.sh
@@ -9,27 +9,27 @@ else
     exit
 fi
 
-if [ $2 == "" ]
+if [ $2 ]
 then
+    topmass="${2/p/.}d0"
+    echo "Top Mass: $topmass"
+else
     echo "Top mass parameter missing!"
     exit
 fi
 
-topmass=$2
-powheginput="powheg.input-tmpl_m${topmass}"
+powheginput="powheg.input-tmpl"
 echo "Generating samples for mass mt = $topmass, file $powheginput"
 
 if [ $1 == 1 ]
 then
 
 # compute in parallel importance sampling grid and upper bounding grid
-    cat ../config/${powheginput} | sed 's/numevts.*/numevts 0/' > powheg.input
+    cat ../config/${powheginput} | sed -e 's/numevts.*/numevts 0/' -e "s/##TOPMASS##/$topmass/g" > powheg.input
     cp ../config/pwgseeds.dat pwgseeds.dat
-    for i in {1..50} #NJOBS
+    for i in {1..250} #NJOBS
     do
-      sed "s/##JOBNUMBER##/$i/g" < ../config/powhegNode.sh-tmpl > powhegNode${i}-tmp.sh
-      sed "s/##PATH##/\/nfs\/dust\/cms\/user\/spanns\/powhegbox\/POWHEG-BOX\/ttJ/g" < powhegNode${i}-tmp.sh > powhegNode${i}.sh
-      rm powhegNode${i}-tmp.sh
+      sed -e "s/##JOBNUMBER##/$i/g" -e "s/##PATH##/\/nfs\/dust\/cms\/user\/spanns\/powhegbox\/POWHEG-BOX\/ttJ/g" < ../config/powhegNode.sh-tmpl > powhegNode${i}.sh
       qsub powhegNode${i}.sh
     done
 
@@ -40,8 +40,8 @@ if [ $1 == 3 ]
     then
 
 # generate the events:
-    cat ../config/${powheginput} > powheg.input
-    for i in {1..50} #NJOBS
+    cat ../config/${powheginput} | sed -e "s/##TOPMASS##/$topmass/g" > powheg.input
+    for i in {1..250} #NJOBS
     do
       qsub powhegNode${i}.sh
     done

c8d045217fff9de1f3f6e0461919985a91cbd67e
Simon Spannagel <simon.spannagel@desy.de>
2016-05-11 15:17:48 +0200

    Increase ntuple job count and increase job importance for submission

--- ntuples/runNTupleGenNafPy8.sh
+++ ntuples/runNTupleGenNafPy8.sh
@@ -11,11 +11,11 @@ fi
 
 run=$1
 
-for i in {1..250} #NJOBS
+for i in {1..501} #NJOBS
 do
   paddedjob=`echo ${i} | awk '{printf("%04s\n", $1)}'`
   sed -e "s/##JOBNUMBER##/${paddedjob}/g" -e "s/##TOPMASS##/${topmass}/g" -e "s/##RUNNAME##/${run}/g" < ../config/cmsntupleNodePy8.sh > cmsntupleNode${i}.sh
-  qsub -N "ntp-m${topmass}" cmsntupleNode${i}.sh
+  qsub -js 2 -N "ntp-m${topmass}" cmsntupleNode${i}.sh
 
 done
 

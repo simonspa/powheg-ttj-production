a65fcab22438088507148e6ecd23a58d953c55f2
Simon Spannagel <simon.spannagel@desy.de>
2016-05-11 15:14:47 +0200

    Update scripts to use 4C tune and higher job importance

--- hadronization/runCMSGenNafPy8.sh
+++ hadronization/runCMSGenNafPy8.sh
@@ -13,12 +13,12 @@ else
 fi
 
 
-for i in {1..250} #NJOBS
+for i in {1..501} #NJOBS
 do
   paddedjob=`echo ${i} | awk '{printf("%04s\n", $1)}'`
-  sed -e "s/##JOBNUMBER##/${paddedjob}/g" -e "s/##MASS##/${mass}/g" -e "s/##TOPMASS##/${topmass}/g" -e "s/##RUNNAME##/${run}/g" < ../config/Hadronizer_Tune4C_emissionVeto1_LHE_pythia8.py-tmpl > Hadronizer_Tune4C_emissionVeto1_LHE_pythia8-${paddedjob}.py
+  sed -e "s/##JOBNUMBER##/${paddedjob}/g" -e "s/##MASS##/${mass}/g" -e "s/##TOPMASS##/${topmass}/g" -e "s/##RUNNAME##/${run}/g" < /nfs/dust/cms/user/spanns/montecarlo-generation/hadronization/config/Hadronizer_Tune4C_emissionVeto1_LHE_pythia8.py-tmpl > Hadronizer_Tune4C_emissionVeto1_LHE_pythia8-${paddedjob}.py
   #sed -e "s/##JOBNUMBER##/${paddedjob}/g" -e "s/##MASS##/${mass}/g" -e "s/##TOPMASS##/${topmass}/g" -e "s/##RUNNAME##/${run}/g" < ../config/Hadronizer_TuneCUEP8_emissionVeto1_LHE_pythia8.py-tmpl > Hadronizer_TuneCUEP8_emissionVeto1_LHE_pythia8-${paddedjob}.py
-  sed -e "s/##JOBNUMBER##/${paddedjob}/g" -e "s/##TOPMASS##/${topmass}/g" -e "s/##RUNNAME##/${run}/g" < ../config/${gennode} > cmsgenNode${i}.sh
-  qsub cmsgenNode${i}.sh
+  sed -e "s/##JOBNUMBER##/${paddedjob}/g" -e "s/##TOPMASS##/${topmass}/g" -e "s/##RUNNAME##/${run}/g" < /nfs/dust/cms/user/spanns/montecarlo-generation/hadronization/config/${gennode} > cmsgenNode${i}.sh
+  qsub -js 2 cmsgenNode${i}.sh
 done
 

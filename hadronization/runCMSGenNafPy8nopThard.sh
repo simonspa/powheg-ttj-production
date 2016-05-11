d0fb190e89607fbdcfe5dc5fb44514304c95777d
Simon Spannagel <simon.spannagel@desy.de>
2016-05-11 15:14:06 +0200

    Add nopThard settings

---
+++ hadronization/runCMSGenNafPy8nopThard.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ $2 ]
+then
+    topmass=$2
+    mass="${topmass/p/.}"
+    run=$1
+    gennode="cmsgenNodePy8.sh-tmpl"
+    echo "Generating samples for mass mt = $topmass, file $gennode"
+else
+    echo "runCMSGenNaf.sh RUNNAME TOPMASS"
+    exit
+fi
+
+
+for i in {1..501} #NJOBS
+do
+  paddedjob=`echo ${i} | awk '{printf("%04s\n", $1)}'`
+  sed -e "s/##JOBNUMBER##/${paddedjob}/g" -e "s/##MASS##/${mass}/g" -e "s/##TOPMASS##/${topmass}/g" -e "s/##RUNNAME##/${run}/g" < /nfs/dust/cms/user/spanns/montecarlo-generation/hadronization/config/Hadronizer_Tune4C_emissionVeto1_LHE_pythia8_nopThard.py-tmpl > Hadronizer_Tune4C_emissionVeto1_LHE_pythia8-${paddedjob}.py
+  #sed -e "s/##JOBNUMBER##/${paddedjob}/g" -e "s/##MASS##/${mass}/g" -e "s/##TOPMASS##/${topmass}/g" -e "s/##RUNNAME##/${run}/g" < ../config/Hadronizer_TuneCUEP8_emissionVeto1_LHE_pythia8.py-tmpl > Hadronizer_TuneCUEP8_emissionVeto1_LHE_pythia8-${paddedjob}.py
+  sed -e "s/##JOBNUMBER##/${paddedjob}/g" -e "s/##TOPMASS##/${topmass}/g" -e "s/##RUNNAME##/${run}/g" < /nfs/dust/cms/user/spanns/montecarlo-generation/hadronization/config/${gennode} > cmsgenNode${i}.sh
+  qsub -js 1 cmsgenNode${i}.sh
+done
+
